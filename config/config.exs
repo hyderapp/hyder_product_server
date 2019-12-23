690b44644722e30ea29125b7c7e35df9e40e66f2
qhwa <qhwa@163.com>
2019-12-23 13:30:44 +0800

    fix missing policy config when deployed

--- config/config.exs
+++ config/config.exs
@@ -40,7 +40,7 @@ config :phoenix_swagger, json_library: Jason
 #
 # config :hps, :archive_storage_path, "priv/archive"
 
-import_config "policy.exs"
+config :hps, :policies, default: HPS.Core.Policy.Default
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
--- config/policy.exs
+++
@@ -1,3 +0,0 @@
-use Mix.Config
-
-config :hps, :policies, default: HPS.Core.Policy.Default
